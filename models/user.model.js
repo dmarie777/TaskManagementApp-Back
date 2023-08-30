2ac884cafa39f2d655fbe4df14846ac3a7c63ddb
ascenDeveloper <eduardofelix014@gmail.com>
2023-08-30 11:59:04 -0400

    I deleted the previous comments to manage and probably start the documentation

--- models/user.model.js
+++ models/user.model.js
@@ -1,21 +1,21 @@
 const mongoose = require("mongoose") // We need to modify the mongoose model or way that our information is stored
 
-const userSchema = new mongoose.Schema({ // Here we create a new Schema or column which helps us to organize data, in this case user, we will create more like this so remember the sintaxis
-        username: { // In this case it's the same to every data but password will change in the future
-            required: true, // We inform our database that our username is strictly necessary
-            type: String // Here we declare the data type of our username and the same with email and password
-        },
-        email: {
-            required: true,
-            type: String
-        },
-        password: {
-            required: true,
-            type: String
-        },
-    })
+const userSchema = new mongoose.Schema({
+    username: {
+        required: true,
+        type: String
+    },
+    email: {
+        required: true,
+        type: String
+    },
+    password: {
+        required: true,
+        type: String
+    },
+})
 
 module.exports = mongoose.model(
     "User",
     userSchema
-) // We need to use the model that we created with mongoose, the syntaxis here is flexible but we will se it in the future, userSchema was declared before and User is the name
+)
--- server.js
+++ server.js
@@ -1,54 +1,42 @@
-// Import the required pre-install packages
 const express = require("express")
 const cors = require("cors")
 const bodyParser = require("body-parser")
 const logger = require("morgan")
-// Add .env files
-const dotenv = require("dotenv") // Necessary for the ussage of .env file which is ignored in .gitignore because of privacy
-dotenv.config() // Without this line dotenv will NOT work
+const dotenv = require("dotenv")
+dotenv.config()
 
 const app = express()
 
-// Port number
 const port = process.env.PORT || 3001
 
-// Set express with the necessary parameter
 app.use(logger("dev"))
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-// Mongoose connection
-const mongoose = require("mongoose") // Mongoose is the ORM( search it ) to write javascript which actually communicate with a database without SQL or another database language
+const mongoose = require("mongoose")
 mongoose.connect(`mongodb://127.0.0.1:27017/taskmanagementapp`,{
                     useNewUrlparser:true,
-                    useUnifiedTopology: true
-}) // Well we need to connect our server to a mongodb server in this case it's a local mongodb server sometimes this link non-locally is similar like mongodb+srv://
-   // The other confiurations are usuallly used to set up a initial configuration of mongodb I usually put that just in case but they are not strictly useful
+                    useUnifiedTopology: true})
     .then(() => {
         console.log("Successfully connect to Mongo")
-    }) // This run just in case our connection to the server was successfully
+    })
     .catch(err => {
         console.error("Connection error", err)
         process.exit()
-    }) // Try to get this error just to see what happen but in simple words tells you that mongoose.connect didn't work
+    })
 
-// Router
 const router = express.Router()
 
-// Routes
-const routes = require('./routes/user.routes') // This is not the best practice but works in case of just manage the user I create it this way because of
-// usefulness to know it's just for users ( this probably change in the future)
-app.use('/api', routes) // We need to apply the changes to our server, in this case we add an /api endpoint
+const routes = require('./routes/user.routes')
+app.use('/api', routes)
 
-// CRUD
 router.get('/', (req, res) => {
     res.json({message: "Hello World!"})
 })
 
 app.use('/',router)
 
-// Express start
 app.listen(port, function() {
     console.log("Running on " + port)
 })
